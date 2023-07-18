8a685b2adf8e78341fe6af9bd40305f2ff317a05
Dmitry Pleshchev <dmitrydevlax@yandex.ru>
2023-07-18 19:50:09 +0500

    Main function added

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@
 *реализовать сортировку по возрасту с использованием индексов
 *реализовать сортировку по возрасту и полу с использованием индексов*/
 
-class User{
+
+/*class User{
   private String fullName;
   private int age;
   private String gender;
@@ -27,4 +28,71 @@ public String getGender() {
 public void displayUser() {
   System.out.println (fullName + " " + age + " " + gender);
 }
+}*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+  private static List users = new ArrayList();
+  private static int currentIndex = 0;
+
+  class User{
+  private String fullName;
+  private int age;
+  private String gender;
+  public User (String fullName, int age, String gender) {
+    this.age = age;
+    this.gender = gender;
+    this.fullName = fullName;
+  }
+public String getFullName() {
+  return fullName;
+}
+public int getAge() {
+  return age;
+}
+
+public String getGender() {
+  return gender;
+}
+
+public void displayUser() {
+  System.out.println (fullName + " " + age + " " + gender);
+}
+}
+  public static void  main (String[] args) {
+    Scanner scanner = new Scanner (System.in);
+
+    while (true) {
+      System.out.println("Введите команду: ");
+      String command = scanner.nextLine().toLowerCase();
+
+      if (command.equals("add")){
+        addUser(scanner);
+      }
+      else if (command.equals("list")) {
+        listUsers();
+      }
+      else if (command.equals("sort by age")) {
+        sortByAge();
+        listUsers();
+      }
+      else if (command.equals("sort by age and gender")) {
+        sortByAgeAndGender();
+        listUsers();
+      }
+      else if (command.equals("exit")) {
+        break;
+      }
+      else{
+        System.out.println("Команда неверная. Попробуйте снова)");
+      }
+    }
+   }
+
+
 }
